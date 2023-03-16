a9279c2fc51107bbbfd009cb588212aab2348f03
SCL/CAT <108624007+ad3109@users.noreply.github.com>
2023-03-16 16:43:38 -0600

    Resurrect EAC Scheduler
    Update EAC scheduler to a usable sol version

---
+++ SchedulerInterfacev01.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+/**
+ * @title SchedulerInterface
+ * @dev The base contract that the higher contracts: BaseScheduler, BlockScheduler and TimestampScheduler all inherit from.
+ */
+contract SchedulerInterface {
+    function schedule(address _toAddress, bytes _callData, uint[8] _uintArgs)
+        public payable returns (address);
+    function computeEndowment(uint _bounty, uint _fee, uint _callGas, uint _callValue, uint _gasPrice)
+        public view returns (uint);
+}
